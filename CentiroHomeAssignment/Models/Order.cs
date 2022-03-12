527a3c1a9f77116c36af5cf2390188919dbcc196
gmendes <goncalo.mendes@altran.com>
2022-03-13 00:27:23 +0100

    Adjust displayed name of columns

--- CentiroHomeAssignment/Models/Order.cs
+++ CentiroHomeAssignment/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,16 +14,30 @@ namespace CentiroHomeAssignment.Models
 
         [Key]
         public int Id { get; set; }
+
+        [DisplayName("Order Number")]
         public int OrderNumber { get; set; }
+
+        [DisplayName("Order Line Number")]
         public int OrderLineNumber { get; set; }
+
+        [DisplayName("Product Number")]
         public string ProductNumber { get; set; }
         public int Quantity { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+
+        [DisplayName("Product Group")]
         public string ProductGroup { get; set; }
+
+        [DisplayName("Order Date")]
         public string OrderDate { get; set; }
+
+        [DisplayName("Customer Name")]
         public string CustomerName { get; set; }
+
+        [DisplayName("Customer Number")]
         public int CustomerNumber { get; set; }
 
     }
