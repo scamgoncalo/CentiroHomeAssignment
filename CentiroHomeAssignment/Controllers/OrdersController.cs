3631dc6c81cf21f08c4f044e82fc3da0dba20d77
gmendes <goncalo.mendes@altran.com>
2022-03-13 00:26:57 +0100

    Add Create action to OrdersController

--- CentiroHomeAssignment/Controllers/OrdersController.cs
+++ CentiroHomeAssignment/Controllers/OrdersController.cs
@@ -15,12 +15,43 @@ namespace CentiroHomeAssignment.Controllers
             _db = db;
         }
 
+        /// <summary>
+        /// Create Order - get request
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Create Order - post request
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create(Order ord)
+        {
+            _db.Order.Add(ord);
+            _db.SaveChanges();
+            // Display all Orders
+            return RedirectToAction("GetAll");
+        }
+
+        /// <summary>
+        /// Get all data from db
+        /// </summary>
+        /// <returns></returns>
         public IActionResult GetAll()
         {
             IEnumerable<Order> orders = _db.Order;
             return View(orders);
         }
 
+        /// <summary>
+        /// Get Orders with same number
+        /// </summary>
+        /// <param name="orderNumber"></param>
+        /// <returns></returns>
         public IActionResult GetByOrderNumber(string orderNumber)
         {
             // TODO: Return the specific order to a view
