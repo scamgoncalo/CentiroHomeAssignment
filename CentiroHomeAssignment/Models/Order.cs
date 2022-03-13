689af4a252fbb0392d52260e532a2974b9c8f52b
gmendes <goncalo.mendes@altran.com>
2022-03-13 12:08:20 +0100

    Update database with parameter date has DateTime

--- CentiroHomeAssignment/Models/Order.cs
+++ CentiroHomeAssignment/Models/Order.cs
@@ -16,28 +16,37 @@ namespace CentiroHomeAssignment.Models
         public int Id { get; set; }
 
         [DisplayName("Order Number")]
+        [Required]
         public int OrderNumber { get; set; }
 
         [DisplayName("Order Line Number")]
+        [Required]
         public int OrderLineNumber { get; set; }
 
         [DisplayName("Product Number")]
+        [Required]
         public string ProductNumber { get; set; }
+
+        [Required]
         public int Quantity { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [Required]
         public double Price { get; set; }
 
         [DisplayName("Product Group")]
+        [Required]
         public string ProductGroup { get; set; }
 
         [DisplayName("Order Date")]
-        public string OrderDate { get; set; }
+        public DateTime OrderDate { get; set; }
 
         [DisplayName("Customer Name")]
         public string CustomerName { get; set; }
 
         [DisplayName("Customer Number")]
+        [Required]
         public int CustomerNumber { get; set; }
 
     }
